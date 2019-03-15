f86d2052dc7126acb89c1914423bde9ddae74eec
Vratislav Kalenda <v.kalenda@gmail.com>
2019-03-15 14:04:45 +0100

    feat: add script for listing recruitee offers into CLI

---
+++ src/Scripts/listOffers.ts
@@ -0,0 +1,11 @@
+import { RecruiteeClient } from "../Recruitee/RecruiteeClient";
+import { getConfig } from "../config";
+import { inspect } from "util";
+
+const config = getConfig();
+
+const recruitee = new RecruiteeClient(config.recruitee!);
+
+recruitee.getOffers().then(o => {
+  console.log(inspect(o, false, 10));
+});

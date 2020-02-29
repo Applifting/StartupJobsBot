a11fb36d6c3ef562dbf0d7233b0529a2fcb2db22
Vratislav Kalenda <v.kalenda@gmail.com>
2020-02-29 11:40:46 +0100

    feat: sanitize error

--- src/Common/deepRegexReplace.ts
+++ src/Common/deepRegexReplace.ts
@@ -1,43 +1,57 @@
-import { isObject,cloneDeep } from "lodash";
-import { isString } from "util";
+import { isObject, clone } from "lodash";
+import { isString, inspect, isError } from "util";
 
 export const deepRegexReplace = (value: any, keysAndValues: Array<string>) => {
-    value = cloneDeep(value);
-    const redactedPlaceholder = '[REDACTED]';
-    if (typeof value === 'undefined' || typeof keysAndValues === 'undefined') return {};
-  
-    if (Array.isArray(value)) {
-      for (let i = 0; i < value.length; i = i + 1) {
-        value[i] = deepRegexReplace(value[i], keysAndValues);
-      }
-      return value;
+  if (!isError(value)) {
+    value = clone(value);
+  } else {
+    value.message = deepRegexReplace(value.message, keysAndValues);
+    value.name = deepRegexReplace(value.name, keysAndValues);
+    if (value.stack) value.stack = deepRegexReplace(value.stack, keysAndValues);
+  }
+
+  const redactedPlaceholder = "[REDACTED]";
+  if (typeof value === "undefined" || typeof keysAndValues === "undefined")
+    return {};
+
+  if (Array.isArray(value)) {
+    for (let i = 0; i < value.length; i = i + 1) {
+      value[i] = deepRegexReplace(value[i], keysAndValues);
     }
-  
-    if (!isObject(value)) {
-      return value;
-    }
-  
-    if (!Array.isArray(keysAndValues)) {
-      return value;
-    }
-  
+    return value;
+  }
+
+  if (isString(value)) {
+    let toReturn = value;
     for (let j = 0; j < keysAndValues.length; j++) {
-      for (let key in value) {
-        if (value.hasOwnProperty(key)) {
-          if (new RegExp(keysAndValues[j],'i').test(key)) value[key] = redactedPlaceholder;
-          if(isString(value[key])){
-            const regex = new RegExp(keysAndValues[j],'gi');
-            value[key] = (value[key] as string).replace(regex, redactedPlaceholder);
-          }
+      const regex = new RegExp(keysAndValues[j], "gi");
+      toReturn = toReturn.replace(regex, redactedPlaceholder);
+    }
+    return toReturn;
+  }
+
+  if (!isObject(value)) {
+    return value;
+  }
+
+  if (!Array.isArray(keysAndValues)) {
+    return value;
+  }
+
+  for (let key in value) {
+    if (value.hasOwnProperty(key)) {
+      let redacted = false;
+      for (let j = 0; j < keysAndValues.length; j++) {
+        if (key === keysAndValues[j]) {
+          value[key] = redactedPlaceholder;
+          redacted = true;
         }
       }
-    }
-  
-    for (let key in value) {
-      if (value.hasOwnProperty(key)) {
+      if (!redacted) {
         value[key] = deepRegexReplace(value[key], keysAndValues);
       }
     }
-  
-    return value;
-  };
+  }
+
+  return value;
+};
---
+++ src/Common/sanitizeError.spec.ts
@@ -0,0 +1,50 @@
+import { sanitzeError } from "./sanitizeError";
+import { AppError } from "./AppError";
+import { Dictionary } from "lodash";
+import { inspect } from "util";
+
+describe("sanitizeError", () => {
+  interface ErrorWithMetadata extends AppError {
+    extraMetaData: Dictionary<any>;
+  }
+
+  it("strips sensistive values from config in the error passed in it", () => {
+    const recruiteeAT = "ABDEFGH4546521AAA123123";
+    const hookUrl =
+      "https://hooks.slack.com/services/T06SD45654/RD45642/ABDaZ894521321";
+    let error = new AppError(
+      400,
+      `Bad thing occured. AccessToken was ${recruiteeAT}`
+    ) as ErrorWithMetadata;
+    error.extraMetaData = {
+      foo: "bar",
+      hookUrl: hookUrl
+    };
+    const sanitizedError = sanitzeError(error, () => {
+      return {
+        server: {
+          logErrors: true,
+          logRequests: true,
+          logPayloads: true,
+          webhookPath: "/some/path",
+          port: 3000
+        },
+        recruitee: {
+          accessToken: recruiteeAT,
+          companyDomain: "Applifting"
+        },
+        slack: {
+          webhookUrl: hookUrl
+        }
+      };
+    });
+
+    expect(sanitizedError.message).toEqual(
+      `Bad thing occured. AccessToken was [REDACTED]`
+    );
+    expect(sanitizedError.extraMetaData).toEqual({
+      foo: "bar",
+      hookUrl: "[REDACTED]"
+    });
+  });
+});

9cc5d5f9ffdd3a802e98c87664206559dbc9eb9f
Vratislav Kalenda <v.kalenda@gmail.com>
2020-03-08 22:38:49 +0100

    chore: cleanup

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -4,24 +4,10 @@ import BotStatus from "../components/BotStatus";
 const Home = () => {
   const { register, handleSubmit, watch, errors } = useForm();
   const current = watch();
-  console.log(current);
-
-  const onSubmit = data => {
-    console.log(data);
-  };
   return (
     <div>
       <h1>Hello world!</h1>
       <BotStatus></BotStatus>
-      <input
-        name="textInput"
-        ref={register({ required: true, maxLength: 50 })}
-      />
-      <div>
-        <a href="https://heroku.com/deploy?template=https://github.com/heroku/node-js-getting-started">
-          <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy" />
-        </a>
-      </div>
     </div>
   );
 };
